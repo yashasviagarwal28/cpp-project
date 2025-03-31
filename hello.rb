ac2dd59d26600d9a4daf65f4c6963df24c423d7f
yashasviagarwal28 <yashasviagarwal28@gmail.com>
2025-03-31 16:12:24 -0400

    Add hello.rb

--- hello.rb
+++ hello.rb
@@ -1,1 +1,22 @@
-print "Hello World"
+#Drawing a shape:
+puts "   /|" #puts is another command for print and it prints out the everything in the order it is coded on
+puts "  / |" #puts also prints out another line
+puts " /  |"
+puts "/___|" #if this was printed in the beginning then this part of the triangle will print first
+print "Hello World" #if printed first then the first puts "   /|" will be printed out with Hello World
+puts ""
+#ruby just prints out eveyrthing in order
+
+
+#Variables:
+#store container data inside variables, to modify we can just refer to the particular var
+#can put the character's name and age and other details in different variables like name, age, etc
+character_name = "John"
+character_age = "35"
+puts ("There once was a man named " + character_name)
+#instead of just doing:    puts "There once was a man named John"
+puts ("he was " + character_age + " years old.")
+puts "He really liked the name John"
+puts "but didn't like being 35."
+
+
